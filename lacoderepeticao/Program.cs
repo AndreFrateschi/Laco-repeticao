680229b771b4fbd1824da07e4754c32c4360c9d8
andre <andrefrateschi@gmail.com>
2020-09-02 18:52:41 -0300

    commit 01

---
+++ lacoderepeticao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lacoderepeticao
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            string nome = "andre";
+
+            Console.WriteLine(nome);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(nome + i);
+            }
+
+
+
+            Console.WriteLine("laco de repeticao!");
+        }
+    }
+}
